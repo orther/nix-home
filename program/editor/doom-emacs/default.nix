157fac0e9db8c7d3330ebad374f09ba2df0a24b6
Brandon Orther <brandon@orther.dev>
2020-06-12 13:49:17 -0700

    Fix editorconfig install for emacs

--- program/editor/doom-emacs/default.nix
+++ program/editor/doom-emacs/default.nix
@@ -9,7 +9,7 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       sbcl
-      editorconfig-checker
+      editorconfig-core-c
       gocode
       gomodifytags
       gotests
